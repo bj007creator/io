9d933ee58505399db295238c527ce290684d9566
mateus-coder1 <letsgood56@gmail.com>
2021-01-02 18:50:41 -0300

    Updates

---
+++ precache-manifest.1ab2dc5a2259826cde0000acda03e86b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502bd5b5a68345acd928a70cdc0dbbce",
+    "url": "/io/index.html"
+  },
+  {
+    "revision": "6e0830d7b030f054e9dd",
+    "url": "/io/static/css/main.6b834948.chunk.css"
+  },
+  {
+    "revision": "66823a5a96c290a6b5ce",
+    "url": "/io/static/js/2.6ed8762c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/io/static/js/2.6ed8762c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d898b78e61397a19c28e",
+    "url": "/io/static/js/3.f4ed500c.chunk.js"
+  },
+  {
+    "revision": "f5246cb66806b628040b",
+    "url": "/io/static/js/4.6ef70b56.chunk.js"
+  },
+  {
+    "revision": "6e0830d7b030f054e9dd",
+    "url": "/io/static/js/main.4cdf385c.chunk.js"
+  },
+  {
+    "revision": "1fe3a293011e6ba202f0",
+    "url": "/io/static/js/runtime-main.2b983688.js"
+  },
+  {
+    "revision": "fe58be3bcab95524b3e4a7fb06500b49",
+    "url": "/io/static/media/logo-dark-border.fe58be3b.png"
+  },
+  {
+    "revision": "4fba58753989ca82b735571e45033792",
+    "url": "/io/static/media/logo-light-border.4fba5875.png"
+  },
+  {
+    "revision": "c0a2f9c00e086e0f3954a88b09eab91d",
+    "url": "/io/static/media/market-graphic.c0a2f9c0.png"
+  }
+]);
